a2d52f8441939354b148f9c82e3ac2c73a3a4ffa
secretscav <secretscav2228@gmail.com>
2022-07-13 13:54:40 +0000

    somthing new __mro__ in python

--- main.py
+++ main.py
@@ -1,2 +1,59 @@
-import itertools
-import inspect
+# python modules
+import os
+
+class A:
+    def __init__(self):
+        print("This is A")
+
+class B(A):
+    def __init__(self):
+        super().__init__()
+        print("this is from B")
+
+class C(A):
+    def __init__(self):
+        super().__init__()
+        print("This is from c")
+
+class B1(B):
+    def __init__(self):
+        super().__init__()
+        print("i am from B1")
+
+class B2(B):
+    def __init__(self):
+        super().__init__()
+        print("i am from B2")
+
+class C1(C):
+    def __init__(self):
+        super().__init__()
+        print("i am from C1")
+
+class C2(C):
+    def __init__(self):
+        super().__init__()
+        print("i am from C2")
+
+
+class D(B1, B2):
+    def __init__(self):
+        super().__init__()
+        print("class D")
+
+class E(C1, C2):
+    def __init__(self):
+        super().__init__()
+        print("class E")
+
+class F(E, D):
+    def __init__(self):
+        super().__init__()
+        print("Hey i am the last one")
+
+
+person = F()
+print(person)
+print(F.__mro__)
+
+
