dcae43c1b41ad75dbf94890e6a2df09aa9a04d10
secretscav <secretscav2228@gmail.com>
2022-07-15 12:59:27 +0000

    decorators in python

--- main.py
+++ main.py
@@ -1,59 +1,12 @@
-# python modules
-import os
+def upper(string:str) -> str:
+    return string.upper()
 
-class A:
-    def __init__(self):
-        print("This is A")
+def lower(string:str) -> str:
+    return string.lower()
 
-class B(A):
-    def __init__(self):
-        super().__init__()
-        print("this is from B")
+def test(func:complex) -> str:
+    Result = func("This is first class python")
+    print(Result)
 
-class C(A):
-    def __init__(self):
-        super().__init__()
-        print("This is from c")
-
-class B1(B):
-    def __init__(self):
-        super().__init__()
-        print("i am from B1")
-
-class B2(B):
-    def __init__(self):
-        super().__init__()
-        print("i am from B2")
-
-class C1(C):
-    def __init__(self):
-        super().__init__()
-        print("i am from C1")
-
-class C2(C):
-    def __init__(self):
-        super().__init__()
-        print("i am from C2")
-
-
-class D(B1, B2):
-    def __init__(self):
-        super().__init__()
-        print("class D")
-
-class E(C1, C2):
-    def __init__(self):
-        super().__init__()
-        print("class E")
-
-class F(E, D):
-    def __init__(self):
-        super().__init__()
-        print("Hey i am the last one")
-
-
-person = F()
-print(person)
-print(F.__mro__)
-
-
+test(upper)
+test(lower)
